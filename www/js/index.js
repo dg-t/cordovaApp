52a7613cd465cee9d2a4fc8edbabfd34eea994ed
orione_90@hotmail.com <orione_90@hotmail.com>
2021-06-26 10:26:41 +0200

    Display device info and set android emulator

--- www/js/index.js
+++ www/js/index.js
@@ -9,6 +9,17 @@ const app = {
         // Cordova is now initialized. Have fun!
         console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
         document.getElementById('deviceready').classList.add('ready');
+
+        // Get the info from device plugins
+        var getDevicelInfo = document.getElementById('device');
+        getDevicelInfo.innerHTML = device.cordova + '<br/>' +
+            device.platform + '<br/>' +
+            device.model + '<br/>' +
+            device.uuid + '<br/>' +
+            device.version + '<br/>' +
+            device.manufacturer + '<br/>' +
+            device.isVirtual + '<br/>' +
+            device.serial + '<br/>';
     }
 }
 
